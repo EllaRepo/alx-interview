de6134048078b745c18b562d00459c482a6304dd
Eliab E <eliaberango@gmail.com>
2024-01-17 23:54:20 -0800

    Refactored the code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,15 +2,20 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error == null) {
+request(url, function (error, response, body) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    printCharactersName(characters, 0);
   }
 });
+
+function printCharactersName (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (error === null) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharactersName(characters, index + 1);
+      }
+    }
+  });
+}
