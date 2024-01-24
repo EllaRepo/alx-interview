fea388d2534cc75b7086f27bf75c605d0eeb37d7
Eliab E <eliaberango@gmail.com>
2024-01-24 12:34:29 -0800

    Add a method that rotates 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module defines a method that rotates 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Function that rotates 2d matrix
+    Args:
+        matrix: 2d matricx
+    """
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
