a907938782a1ae80fa759efea63b8a57b7ec8ef9
Eliab E <eliaberango@gmail.com>
2024-01-30 09:29:50 -0800

    Add 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module defines a method that returns fewest number of coins
+    needed to meet total
+"""
+
+
+def makeChange(coins, total):
+    """Return fewest number of coins needed to meet total
+    Args:
+        coins: a list of the values of the coins in your possession
+        total: given amount
+    Returns:
+        0 - if total is 0 or less,
+        -1 - If total cannot be met by any number of coins
+    """
+    if total < 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
