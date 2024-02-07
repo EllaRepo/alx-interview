e856dff10f2ba88b0513c92d32d383db11cbe4b6
Eliab E <eliaberango@gmail.com>
2024-02-07 03:53:22 -0800

    Refactored the code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,10 +21,9 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                perimeter += 4
-                if r > 0 and grid[r - 1][c] == 1:
-                    perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:
-                    perimeter -= 2
+                if r > 0 and grid[r - 1][c] == 0:
+                    perimeter += 2
+                if c > 0 and grid[r][c - 1] == 0:
+                    perimeter += 2
 
     return perimeter
